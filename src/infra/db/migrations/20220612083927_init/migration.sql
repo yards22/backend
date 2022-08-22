11be95a932a056788c016c75e0beffb5d6dff294
himanshu-sah <itsmehs07@gmail.com>
2022-08-22 22:12:15 +0530

    prisma gen

--- src/infra/db/migrations/20220612083927_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "services" (
-    "id" BIGSERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "description" TEXT,
-    "created_at" BIGINT NOT NULL DEFAULT extract(epoch FROM now()),
-
-    CONSTRAINT "services_pkey" PRIMARY KEY ("id")
-);
--- src/infra/db/migrations/20220812040209_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "services" ALTER COLUMN "created_at" SET DEFAULT extract(epoch FROM now());
