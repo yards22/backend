2cbfee1bc97eb0cab5400e448827306c455b1aad
himanshu-sah <itsmehs07@gmail.com>
2022-08-23 00:09:26 +0530

    kv store implementation for redis

---
+++ src/pkg/kv_store/redis.ts
@@ -0,0 +1,66 @@
+import { IKVStore } from "./kv_store";
+import { createClient, RedisClientType } from "redis";
+class Redis implements IKVStore {
+  store: RedisClientType;
+  constructor(host: string, port: number, password: string) {
+    this.store = createClient({
+      url: `redis://${host}:${port}`,
+      password,
+    });
+  }
+  Set(key: string, value: string, expiryTime?: number): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.store
+        .set(key, value, {
+          EX: expiryTime,
+        })
+        .then(() => {
+          return resolve();
+        })
+        .catch((err) => {
+          return reject(err);
+        });
+    });
+  }
+  Get(key: string): Promise<string | null> {
+    return new Promise((resolve, reject) => {
+      this.store
+        .get(key)
+        .then((v) => {
+          return resolve(v);
+        })
+        .catch((err) => {
+          return reject(err);
+        });
+    });
+  }
+  Delete(key: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.store
+        .del(key)
+        .then(() => {
+          return resolve();
+        })
+        .catch((err) => {
+          return reject(err);
+        });
+    });
+  }
+  Truncate(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.store
+        .flushDb()
+        .then(() => {
+          return resolve();
+        })
+        .catch((err) => {
+          return reject(err);
+        });
+    });
+  }
+  Close() {
+    this.store.disconnect();
+  }
+}
+
+export default Redis;
