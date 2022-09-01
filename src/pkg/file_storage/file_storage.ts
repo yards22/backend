779cc61e89fe5fab867198aeca562bcc3a0b8b82
himanshu-sah <itsmehs07@gmail.com>
2022-09-01 14:04:25 +0530

    abstraction over file system, file storage init

---
+++ src/pkg/file_storage/file_storage.ts
@@ -0,0 +1,6 @@
+export interface FileStorage {
+  Put(filePath: string, fileData: any): Promise<void>;
+  Get(filePath: string): Promise<Buffer>;
+  Delete(filePath: string): Promise<void>;
+  IfFileExists(filePath: string): Promise<boolean>;
+}
---
+++ src/pkg/file_storage/local_file_storage.ts
@@ -0,0 +1,36 @@
+import { FileStorage } from "./file_storage";
+import { readFile, writeFile, unlink, access } from "fs";
+export class LocalFileStorage implements FileStorage {
+  Put(filePath: string, fileData: any): Promise<void> {
+    return new Promise((resolve, reject) => {
+      writeFile(filePath, fileData, (err) => {
+        if (err) return reject(err);
+        resolve();
+      });
+    });
+  }
+  Get(filePath: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      readFile(filePath, (err, data) => {
+        if (err) return reject(err);
+        resolve(data);
+      });
+    });
+  }
+  Delete(filePath: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      unlink(filePath, (err) => {
+        if (err) return reject(err);
+        resolve();
+      });
+    });
+  }
+  IfFileExists(filePath: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      access(filePath, (err) => {
+        if (err) return resolve(false);
+        resolve(true);
+      });
+    });
+  }
+}
