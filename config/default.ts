7e2db0686ab24d4f50c01e5765e4b9147feedb49
Rithvik89 <innovation6178@gmail.com>
2022-09-04 00:35:24 +0530

    added origin key-value in config

--- config/default.ts
+++ config/default.ts
@@ -2,5 +2,6 @@ export default {
     "service":"outlook.com",
     "mailId":"project22.yards@outlook.com",
     "password":"nitwarangal@3",
-    "SMTPport":587
+    "SMTPport":587,
+    "origin":"http://localhost:3000"
 };
