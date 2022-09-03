813898b4f23ae127a29ea42bc8dda8ce3c8a2b95
Adarsh <adarsh333reddy@gmail.com>
2022-09-03 15:52:35 +0530

    fixed minor bugs in profile

--- src/cmd/http_api/profile.ts
+++ src/cmd/http_api/profile.ts
@@ -4,20 +4,18 @@ import RouteHandler from "./types";
 
 export const HandleCreateProfile: RouteHandler = (req, res, next, app) => {
   const userId = BigInt(req.context.userId);
+  const userName: string = req.body.username as string;
+  const emailId: string = req.body.email_id as string;
+  const profileImage: string = req.body.profile_image as string;
+  const bio: string = req.body.bio as string;
 
-  const userName: string = req.body.username;
-  const emailId: string = req.body.email_id;
-  const profileImage: string = req.body.profile_image;
-  const registeredDate: Date = new Date();
-  const bio: string = req.body.bio;
-  const cricIndex = req.body.cric_index;
-  if (userName == undefined || emailId == undefined) {
-    app.SendRes(res, { status: 422, data: "invalid credentials" });
+  if ( userName == undefined || emailId == undefined ) {
+    app.SendRes(res, { status: HerrorStatus.StatusUnprocessableEntity, data: "invalid credentials" });
   }
   app.profileManager
     .CreateProfile(userId, userName, bio, profileImage, emailId)
     .then(() => {
-      app.SendRes(res, { status: 200, data: "profile created successfully" });
+      app.SendRes(res, { status: HerrorStatus.StatusOK, data: "profile created successfully" });
     })
     .catch((err) => {
       next(err);
@@ -51,7 +49,7 @@ export const HandleGetUserProfile: RouteHandler = (req, res, next, app) => {
   app.profileManager
     .GetUserById(userId)
     .then((profile) => {
-      app.SendRes(res, { status: 200, data: profile });
+      app.SendRes(res, { status: HerrorStatus.StatusOK, data: profile });
     })
     .catch((err) => {
       next(err);
@@ -66,22 +64,7 @@ export const HandleUpdateProfile: RouteHandler = (req, res, next, app) => {
     .UpdateProfile(userId, undefined, bio)
     .then((profile) => {
       // returning the updated profile
-      app.SendRes(res, { status: 200, data: profile });
-    })
-    .catch((err) => {
-      next(err);
-    });
-  return;
-};
-
-export const HandleUpdateProfileImage: RouteHandler = (req, res, next, app) => {
-  const userId = BigInt(req.context.userId);
-  const bio: string = req.body.bio as string;
-  app.profileManager
-    .UpdateProfile(userId, undefined, bio)
-    .then((profile) => {
-      // returning the updated profile
-      app.SendRes(res, { status: 200, data: profile });
+      app.SendRes(res, { status: HerrorStatus.StatusOK, data: profile });
     })
     .catch((err) => {
       next(err);
