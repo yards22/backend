da6a44ec8084e010ac37ae9bfabde10e2628c566
Adarsh <adarsh333reddy@gmail.com>
2022-10-09 10:57:40 +0530

    updated schema to include feedback table

--- src/infra/db/migrations/20220929141647_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `posts` ADD COLUMN `is_private` BOOLEAN NOT NULL DEFAULT false;
--- src/infra/db/migrations/20221007054139_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `posts` ADD COLUMN `edits` INTEGER NOT NULL DEFAULT 0;
