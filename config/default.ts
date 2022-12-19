354ed94ca5918be68edd4b5f60684a7460edb794
himanshu-sah <itsmehs07@gmail.com>
2022-12-19 13:53:29 +0530

    change react path

--- config/default.ts
+++ config/default.ts
@@ -3,7 +3,7 @@ export default {
   mailId: "project22.yards@outlook.com",
   password: "nitwarangal@3",
   SMTPport: 587,
-  origin: "http://localhost:3000",
+  origin: "https://stage.d3es52rpkdll31.amplifyapp.com",
   googleClientId:
     "442538559529-bm10qpqtcg3k06rrgnc4i0pqnc3fee4s.apps.googleusercontent.com",
 };
