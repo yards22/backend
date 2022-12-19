765c434626105eef19907b533baa265921f88949
himanshu-sah <itsmehs07@gmail.com>
2022-12-19 19:53:47 +0530

    updated build.yml with deploy job

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 # This is a comment!
 echo Starting to Deploy ....
-npm install
-mkdir dist
+yarn
 npx prisma generate --schema ./src/infra/db/schema.prisma
 npx prisma migrate deploy --schema ./src/infra/db/schema.prisma
-npm run build
