5e4f2572d4f324d43a7f450831f14bdb8f99a2c4
himanshu-sah <itsmehs07@gmail.com>
2022-12-19 18:48:47 +0530

    update script to deploy the application

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,5 @@ echo Starting to Deploy ....
 npm install
 mkdir dist
 npx prisma generate --schema ./src/infra/db/schema.prisma
-npx prisma migrate dev -n init --schema ./src/infra/db/schema.prisma
+npx prisma migrate deploy --schema ./src/infra/db/schema.prisma
 npm run build
-node dist/src/cmd/http_api/main.js
