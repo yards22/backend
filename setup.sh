b6ab9e35fdd31fa49abfa2201bae645831558471
rithvik <innovation6178@gmail.com>
2024-01-26 13:04:33 +0530

    Network added

--- setup.sh
+++ setup.sh
@@ -2,10 +2,9 @@
 # This is a comment!
 echo Local Setup 
 docker rm -f node_mysql
-docker run --name node_mysql -p 3456:3306 -e MYSQL_ROOT_PASSWORD=mypassword -d mysql:8  
+docker run  --name node_mysql --network mynetwork -p 3456:3306 -e MYSQL_ROOT_PASSWORD=mypassword -d mysql:8  
 docker rm -f node_redis	
 docker run --name node_redis -p 6379:6379 -d redis
 # sleep 20
 # npx prisma migrate dev -n init --schema ./src/infra/db/schema.prisma
-
 
